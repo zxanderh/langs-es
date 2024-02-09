2f2410d81d93727806af1eca1c0345ab2dc5ddb7
Zane Huston <zane@dentimax.com>
2024-02-09 12:30:27 -0700

    chore: skip esm test for node versions <13

--- test/esm/esm.test.js
+++ test/esm/esm.test.js
@@ -3,6 +3,9 @@ import { assert } from 'chai';
 import sharedTests from '../sharedTests.cjs';
 
 describe('ESM', function () {
-  // eslint-disable-next-line mocha/no-setup-in-describe
-  sharedTests(langs, assert);
+  /* eslint-disable mocha/no-setup-in-describe */
+  if (process.versions && process.versions.node && +process.versions.node.slice(0, 2) >= 13) {
+    sharedTests(langs, assert);
+  }
+  /* eslint-enable mocha/no-setup-in-describe */
 });
